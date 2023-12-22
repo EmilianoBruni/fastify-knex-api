6c929bf407637092980245ef70af95727b33a718
Emiliano Bruni <info@ebruni.it>
2023-12-22 11:59:55 +0100

    Correct API initialization

--- src/API.js
+++ src/API.js
@@ -1,11 +1,16 @@
 // Class that handles all API calls
 
+import DefaultController from './DefaultController.js';
+import crudGen from 'fastify-crud-generator';
 import { SchemaInspector } from 'knex-schema-inspector';
 
 class API {
     constructor(params = {}) {
+        this._fastify = params.fastify;
         this._knex = params.knex;
         this._tables = params.tables;
+
+        this.schemaInspector = SchemaInspector(this._knex);
 
         if (!this._tables) {
             this.isInizialized = this._getDBTables().then(tables => {
@@ -14,18 +19,23 @@ class API {
                 this.initialize();
             });
         } else {
-            this.isInizialized = Promise.resolve();
             this.initialize();
+            this.isInizialized = Promise.resolve();
         }
     }
 
     initialize() {
-        console.log('Initializing API');
+        // register routes for each table
+        for (const table of this._tables) {
+            this._fastify.register(crudGen, {
+                prefix: `/api/${table}`,
+                controller: new DefaultController(table)
+            });
+        }
     }
 
     async _getDBTables() {
-        console.log('Getting tables');
-        return await SchemaInspector(this._knex).tables();
+        return await this.schemaInspector.tables();
     }
 }
 
