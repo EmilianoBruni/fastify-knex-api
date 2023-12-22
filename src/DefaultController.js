d63a137b4050622afbecfcde6407dd0eadaf4e7e
Emiliano Bruni <info@ebruni.it>
2023-12-22 12:00:21 +0100

    Update with right knex call

--- src/DefaultController.js
+++ src/DefaultController.js
@@ -1,22 +1,23 @@
 class DefaultController {
-    constructor(tableName, pk = 'id') {
+    constructor(tableName, pk = undefined) {
         this.table = tableName;
         this.pk = pk;
     }
 
     async list(req, reply) {
-        const [data, fields] = await req.server.mysql.query(
-            `SELECT * FROM ${this.table} limit 10`
-        );
-        return data;
+        await this._fillPkIfUndefined(req);
+        const total = await this._count(req.server.knex(this.table));
+        const items = await req.server.knex(this.table)
+            .select('*')
+        return this._formatManyResult(total, items);
     }
 
     async view(req, reply) {
         const id = req.params.id;
-        const [data, fields] = await req.server.mysql.query(
-            `SELECT * FROM ${this.table} WHERE ${this.pk} = ?`,
-            [id]
-        );
+        await this._fillPkIfUndefined(req);
+        const data = await req.server.knex(this.table)
+            .select('*')
+            .where(this.pk, id);
         if (data.length === 0) {
             reply.code(404).send({ message: 'Not found' });
         }
@@ -24,29 +25,62 @@ class DefaultController {
     }
 
     async create(req, reply) {
-        const { nome, cognome, email, password } = req.body;
-        return await req.server.mysql.query(
-            `INSERT INTO ${this.table} (nome, cognome, email, password) VALUES (?, ?, ?, ?)`,
-            [nome, cognome, email, password]
-        );
+        let data;
+        try {
+            data = await req.server.knex(this.table)
+                .insert(req.body);
+        } catch (err) {
+            reply.code(500).send({ message: 'Error', err: err });
+        }
+        return data[0];
     }
 
     async update(req, reply) {
         const id = req.params.id;
-        const { nome, cognome, email, password } = req.body;
-        return await req.server.mysql.query(
-            `UPDATE ${this.table} SET nome = ?, cognome = ?, email = ?, password = ? WHERE ${this.pk} = ?`,
-            [nome, cognome, email, password, id]
-        );
+        await this._fillPkIfUndefined(req);
+        let data;
+        try {
+            data = await req.server.knex(this.table)
+                .update(req.body)
+                .where(this.pk, id);
+        } catch (err) {
+            reply.code(500).send({ message: 'Error', err: err });
+        };
+        if (data === 0) {
+            reply.code(404).send({ message: 'Not found' });
+        }
+        return {affected: data};
     }
 
     async delete(req, reply) {
         const id = req.params.id;
-        return await req.server.mysql.query(
-            `DELETE FROM ${this.table} WHERE ${this.pk} = ?`,
-            [id]
-        );
+        await this._fillPkIfUndefined(req);
+        const data = await req.server.knex(this.table)
+            .del()
+            .where(this.pk, id);
+        if (data === 0) {
+            reply.code(404).send({ message: 'Not found' });
+        }
+        return {affected: data};
     }
+
+    async _count(knex) {
+        const count = await knex.count(`${this.pk} as count`);
+        return count[0].count;
+    }
+
+    _formatManyResult(total, items) {
+        return { total: total, items: items };
+    }
+
+    async _fillPkIfUndefined(req) {
+        if (this.pk === undefined) {
+            this.pk = await req.server.knexAPI.schemaInspector.primary(
+                this.table
+            );
+        }
+    }
+
 }
 
 export default DefaultController;
