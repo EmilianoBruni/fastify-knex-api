408700ab2aa30fda10f64fb20c9ee2ace820e698
Emiliano Bruni <info@ebruni.it>
2023-12-22 13:07:49 +0100

    Test prefix options

---
+++ test/020-prefix.test.js
@@ -0,0 +1,29 @@
+import t from 'tap';
+import { createServer } from './helpers.js';
+
+const prefix = '/crud'
+
+t.test('Use a different prefix', async t => {
+    const app = await createServer(t, { prefix: prefix });
+
+    t.test('Get all authors', async t => {
+        const res = await app.inject({
+            method: 'GET',
+            url: `${prefix}/authors`
+        });
+        t.equal(res.statusCode, 200);
+        t.equal(
+            res.headers['content-type'],
+            'application/json; charset=utf-8'
+        );
+        // check if the response is Object like {total: integer, items: [...]}
+        t.ok(typeof res.json() === 'object');
+        t.ok(typeof res.json().total === 'number');
+        t.ok(Array.isArray(res.json().items));
+        // check total = items.length
+        t.equal(res.json().total, res.json().items.length);
+
+    });
+});
+
+t.end();
