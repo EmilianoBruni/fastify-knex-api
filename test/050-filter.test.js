f844df349feb86985ba7687ec83347e698975af2
Emiliano Bruni <info@ebruni.it>
2023-12-28 10:27:00 +0100

    lint:fix

--- test/050-filter.test.js
+++ test/050-filter.test.js
@@ -1,15 +1,15 @@
-import { createServer } from "./helpers.js";
-import t from "tap";
+import { createServer } from './helpers.js';
+import t from 'tap';
 
-t.test("Check filtering", async t => {
+t.test('Check filtering', async t => {
     const app = await createServer(t);
 
-    t.test("Filter by id", async t => {
+    t.test('Filter by id', async t => {
         const res = await app.inject({
-            method: "GET",
-            url: "/api/authors",
+            method: 'GET',
+            url: '/api/authors',
             query: {
-                filter: "id=4"
+                filter: 'id=4'
             }
         });
         t.equal(res.statusCode, 200);
@@ -17,10 +17,10 @@ t.test("Check filtering", async t => {
         t.equal(res.json().items[0].id, 4);
     });
 
-    t.test("Filter by name", async t => {
+    t.test('Filter by name', async t => {
         const res = await app.inject({
-            method: "GET",
-            url: "/api/authors",
+            method: 'GET',
+            url: '/api/authors',
             query: {
                 filter: "first_name='Nunzia'"
             }
@@ -28,29 +28,29 @@ t.test("Check filtering", async t => {
         t.equal(res.statusCode, 200);
         t.equal(res.json().items.length, 1);
         t.equal(res.json().items[0].id, 7);
-        t.equal(res.json().items[0].first_name, "Nunzia");
+        t.equal(res.json().items[0].first_name, 'Nunzia');
     });
 
-    t.test("Filter by id less than", async t => {
+    t.test('Filter by id less than', async t => {
         const res = await app.inject({
-            method: "GET",
-            url: "/api/authors",
+            method: 'GET',
+            url: '/api/authors',
             query: {
-                filter: "id<=4"
+                filter: 'id<=4'
             }
         });
         t.equal(res.statusCode, 200);
         const json = res.json();
         t.equal(json.items.length, 4);
-        [1, 2, 3, 4].forEach((id) => {
+        [1, 2, 3, 4].forEach(id => {
             t.equal(json.items[id - 1].id, id);
         });
     });
 
-    t.test("And filter with no result", async t => {
+    t.test('And filter with no result', async t => {
         const res = await app.inject({
-            method: "GET",
-            url: "/api/authors",
+            method: 'GET',
+            url: '/api/authors',
             query: {
                 filter: "id<=4 AND first_name='Gabriele'"
             }
@@ -60,10 +60,10 @@ t.test("Check filtering", async t => {
         t.equal(json.items.length, 0);
     });
 
-    t.test("And filter with one result", async t => {
+    t.test('And filter with one result', async t => {
         const res = await app.inject({
-            method: "GET",
-            url: "/api/authors",
+            method: 'GET',
+            url: '/api/authors',
             query: {
                 filter: "id<=4 AND first_name='Deborah'"
             }
@@ -74,10 +74,10 @@ t.test("Check filtering", async t => {
         t.equal(json.items[0].id, 3);
     });
 
-    t.test("And and or filters with two results", async t => {
+    t.test('And and or filters with two results', async t => {
         const res = await app.inject({
-            method: "GET",
-            url: "/api/authors",
+            method: 'GET',
+            url: '/api/authors',
             query: {
                 filter: "id<=4 AND (first_name='Deborah' OR first_name='Quasimodo')"
             }
@@ -88,13 +88,13 @@ t.test("Check filtering", async t => {
         t.equal(json.items[0].id, 3);
         t.equal(json.items[1].id, 4);
     });
-    t.test("And and or filters with two results and sort", async t => {
+    t.test('And and or filters with two results and sort', async t => {
         const res = await app.inject({
-            method: "GET",
-            url: "/api/authors",
+            method: 'GET',
+            url: '/api/authors',
             query: {
                 filter: "id<=4 AND (first_name='Deborah' OR first_name='Quasimodo')",
-                sort: "-first_name"
+                sort: '-first_name'
             }
         });
         t.equal(res.statusCode, 200);
@@ -104,12 +104,12 @@ t.test("Check filtering", async t => {
         t.equal(json.items[1].id, 3);
     });
 
-    t.test("Filter over boolean field", async t => {
+    t.test('Filter over boolean field', async t => {
         const res = await app.inject({
-            method: "GET",
-            url: "/api/authors",
+            method: 'GET',
+            url: '/api/authors',
             query: {
-                filter: "active=true"
+                filter: 'active=true'
             }
         });
         t.equal(res.statusCode, 200);
@@ -117,10 +117,10 @@ t.test("Check filtering", async t => {
         t.equal(json.items.length, 8);
 
         const res2 = await app.inject({
-            method: "GET",
-            url: "/api/authors",
+            method: 'GET',
+            url: '/api/authors',
             query: {
-                filter: "active=false"
+                filter: 'active=false'
             }
         });
         t.equal(res2.statusCode, 200);
